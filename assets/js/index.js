e51e1146ede27017a0a1412143ace950f3a78fd7
anlongawf <88261950+anlongawf@users.noreply.github.com>
2024-09-17 17:31:29 +0700

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('body,html').animate({
+      scrollTop: $(hash).offset().top
+      }, 1200, function(){
+      window.location.hash = hash;
+     });
+     } 
+    });
+});
+
+var width = $(window).width(); 
+
+window.onscroll = function(){
+if ((width >= 900)){
+    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        $("#middle").css("background-size","150% auto");
+    }else{
+        $("#middle").css("background-size","100% auto");        
+    }
+}
+};
+
+setTimeout(function(){
+    $("#loading").addClass("animated fadeOut");
+    setTimeout(function(){
+      $("#loading").removeClass("animated fadeOut");
+      $("#loading").css("display","none");
+    },800);
+},1450);
+
+
+// Nav mobile menu
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+
+
+// Alert
+function loginFail() {
+  var alertBox = document.getElementById("customAlert");
+  alertBox.style.display = "block";
+  alertBox.classList.add("fade-in");
+}
+
+// Đóng thông báo
+function closeAlert() {
+  var alertBox = document.getElementById("customAlert");
+  alertBox.style.display = "none";
+}
